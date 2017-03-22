818cc1394d00d8b7232cfa2ead6d775898c1d669
AnaColovic <ana.colovic996@gmail.com>
2017-03-23 00:50:47 +0100

    Napravljene klase Kurs i Valuta sa svojim atributima i get i set metodama

---
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,42 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+	private double prodajni;
+	private double kupovni;
+	private double srednji;
+	private GregorianCalendar datum;
+	private Valuta valuta;
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public Valuta getValuta() {
+		return valuta;
+	}
+	public void setValuta(Valuta valuta) {
+		this.valuta = valuta;
+	}
+	
+}
