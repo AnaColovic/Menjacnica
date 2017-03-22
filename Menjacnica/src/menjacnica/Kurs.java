84b07787a37443c32d87ee7a8a4128f2d1d94273
AnaColovic <ana.colovic996@gmail.com>
2017-03-23 00:53:52 +0100

    U klase Kurs i Valuta dodate metode toString, equals i hashCode

--- Menjacnica/src/menjacnica/Kurs.java
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -38,5 +38,39 @@ public class Kurs {
 	public void setValuta(Valuta valuta) {
 		this.valuta = valuta;
 	}
+	@Override
+	public String toString() {
+		return "Kurs [prodajni=" + prodajni + ", kupovni=" + kupovni + ", srednji=" + srednji + ", datum=" + datum
+				+ ", valuta=" + valuta + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
 	
 }
