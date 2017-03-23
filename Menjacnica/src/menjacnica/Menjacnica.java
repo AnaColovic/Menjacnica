f6a64254356d41ad23a49aaed43b6e872db4bec9
AnaColovic <ana.colovic996@gmail.com>
2017-03-23 01:28:31 +0100

    Uvedena logicka kontrola u metodu dodajKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,13 +1,20 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.InterfejsMenjacnica;
 
 public class Menjacnica implements InterfejsMenjacnica{
+	private LinkedList<Kurs> kursnaLista = new LinkedList<Kurs>();
 
 	@Override
 	public void dodajKurs(Kurs k) {
+		if(k==null) {
+			throw new RuntimeException("Morate uneti kurs da biste ga dodali u kursnu listu");
+		} else {
+			kursnaLista.add(k);
+		}
 	}
 
 	@Override
