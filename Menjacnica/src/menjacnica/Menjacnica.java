32839906671946bdd89d7a3c03ec7f5a0a674c2f
AnaColovic <ana.colovic996@gmail.com>
2017-03-23 01:16:02 +0100

    Napravljena klasa Menjacnica koja implementira interfejs InterfejsMenjacnica

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,22 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.InterfejsMenjacnica;
+
+public class Menjacnica implements InterfejsMenjacnica{
+
+	@Override
+	public void dodajKurs(Kurs k) {
+	}
+
+	@Override
+	public void obrisiKurs(Kurs k) {
+	}
+
+	@Override
+	public Kurs vratiKurs(Valuta v, GregorianCalendar datum) {
+		return null;
+	}
+
+}
